44f5d6f402583281a33a898dc70805275d9cd9c0
JoseAntonio98 <sawm5798@gmail.com>
2021-12-10 15:48:33 -0500

    Cambios para mejorar la responsividad

--- src/components/UsersComponents/Cliente/Login.tsx
+++ src/components/UsersComponents/Cliente/Login.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from 'react-router-dom';
-import {IonCol, IonButton, IonContent, IonGrid, IonRow, IonTitle, IonItem, IonLabel, IonInput } from '@ionic/react';
-import { signInCliente } from '../../../firebaseCliente';
+import {IonCol, IonButton, IonIcon, IonGrid, IonRow, IonTitle, IonItem, IonLabel, IonInput } from '@ionic/react';
+import { signInCliente } from '../../../servicios/firebaseCliente';
 import './Login.css';
+
+import Sesion from "../../../Contexto/Sesion"
+
+import {personCircle} from 'ionicons/icons'
 
 interface ContainerProps { }
 
 const Login: React.FC<ContainerProps> = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const uid = useContext(Sesion)
+    console.log(uid)
 
     async function login() {
         await signInCliente(email, password)
@@ -17,42 +24,42 @@ const Login: React.FC<ContainerProps> = () => {
     return (
             <IonGrid>
                 <IonRow>
-                    <IonCol></IonCol>
-                    <IonCol className='ion-text-center ion-margin-top'>
+                    <IonCol offsetLg="4" sizeLg="4" className='ion-text-center ion-margin-top'>
                         <h1>Ingresar como Cliente</h1>
                     </IonCol>
-                    <IonCol></IonCol>
                 </IonRow>
                 <IonRow>
-                    <IonCol></IonCol>
-                    <IonCol>
+                    <IonCol offsetLg="4" sizeLg="4" className='ion-text-center ion-margin-top'>
+                        <IonIcon
+                        style={{ fontSize: "70px", color: "#0040ff" }}
+                        icon={personCircle}
+                        />
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol offsetLg="4" sizeLg="4">
                         <IonItem>
                             <IonLabel position="floating" > Correo Electronico</IonLabel>
                             <IonInput onIonChange={(e:any) => setEmail(e.target.value)} type="email"/>
                         </IonItem>
                     </IonCol>
-                    <IonCol></IonCol>
                 </IonRow>
                 <IonRow>
-                    <IonCol></IonCol>
-                    <IonCol>
+                    <IonCol offsetLg="4" sizeLg="4">
                         <IonItem>
                             <IonLabel position="floating"> Contraseña</IonLabel>
                             <IonInput onIonChange={(e:any) => setPassword(e.target.value)} type="password"/>
                         </IonItem>
                     </IonCol>
-                    <IonCol></IonCol>
                 </IonRow>
                 <IonRow>
-                <IonCol></IonCol>
-                    <IonCol className='ion-text-center ion-margin-top'>
+                    <IonCol offsetLg="4" sizeLg="4" className='ion-text-center ion-margin-top'>
                         <IonButton expand="block" fill="outline" onClick={login} >
                             Ingresar
                         </IonButton>
                         <p><Link to="/tienda/login">soy una tienda</Link></p>
-                        <p> <Link to="/cliente/registro">¿Eres nuevo aquí? </Link></p>
+                        <p> ¿Eres nuevo aquí? <Link to="/cliente/registro">Registrate</Link> </p>
                     </IonCol>
-                    <IonCol></IonCol>
                 </IonRow>
             </IonGrid>
     );

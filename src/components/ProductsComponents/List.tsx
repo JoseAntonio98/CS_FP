5eff3ace326c6836d83cf7907836e8f4612e8df1
JoseAntonio98 <sawm5798@gmail.com>
2021-12-11 00:48:48 -0500

    Mostrar datos de Productos cercanos

--- src/components/ProductsComponents/List.tsx
+++ src/components/ProductsComponents/List.tsx
@@ -1,36 +1,66 @@
-import { IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonNav, IonRow, IonTitle } from '@ionic/react';
+import { IonCol, IonLoading, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonNav, IonRow, IonTitle } from '@ionic/react';
 import ProductInfo from '../ProductsComponents/Product/ProductInfo'
+import { db } from '../../firebaseConfig'
+import { useState, useEffect } from 'react';
+import { collection, getDoc, getDocs, onSnapshot, query, } from "firebase/firestore";
 
-interface ContainerProps { }
+interface ContainerProps
+{
+}
 
-const List: React.FC<ContainerProps> = () => {
-  return (
-    <IonRow>
-        <IonCol>
-        <IonContent scrollY={true} fullscreen>
-            <IonTitle>Tiendas Cercanas</IonTitle>
-            <IonGrid>
-                <IonRow>
-                    <IonCol>
-                        <ProductInfo/>
-                    </IonCol>
-                </IonRow>
-                <IonRow>
-                    <IonCol>
-                        <ProductInfo/>
-                    </IonCol>
-                </IonRow>
-                <IonRow>
-                    <IonCol>
-                        <ProductInfo/>
-                    </IonCol>
-                </IonRow>
-            </IonGrid>
-        </IonContent>
-        </IonCol>
-    </IonRow>
-    
-  )
+const ProductoList: React.FC<ContainerProps> = () => {
+
+    const [arrayProductos, setArrayProductos] = useState([{}]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+
+    const productosCollectionRef = collection(db, 'Producto')
+    useEffect(() => {
+        async function obtenerProductos()
+        {
+            const data = await getDocs(productosCollectionRef);
+            setArrayProductos(data.docs.map((doc) => (
+                {...doc.data(), 
+                    id: doc.id}
+            )))
+            setLoading(false)
+
+        }
+        obtenerProductos()
+    }, [loading])
+
+    if (loading) {
+        return <IonLoading
+        isOpen={loading}
+        onDidDismiss={() => setLoading(false)}
+        message={'Obteniendo Tiendas Cercanas'}
+    />
+    }
+
+    return (
+        <IonRow>
+            <IonCol>
+                <IonContent scrollY={true} fullscreen>
+                    <IonTitle>Tiendas Cercanas</IonTitle>
+                    <IonGrid>
+                        {
+                            arrayProductos.length > 0 ?
+                            arrayProductos.map((item:any, index:Number) => {
+                                return (
+                                    <IonRow key={index.toString()}>
+                                        <IonCol>
+                                            <ProductInfo  productoNombre={item.nombre} docId={item.id} productoSede='' image=""/>
+                                        </IonCol>
+                                    </IonRow>
+                                )
+                            }) : null
+                        }
+                    </IonGrid>
+                </IonContent>
+            </IonCol>
+        </IonRow>
+
+    )
 };
 
-export default List;
+export default ProductoList;

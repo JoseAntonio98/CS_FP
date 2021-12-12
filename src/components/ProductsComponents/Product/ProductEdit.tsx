575e2e8e08483a98b63a67f8780dfb0edcdd7256
mmendorod <41299072+mmendorod@users.noreply.github.com>
2021-12-12 17:48:14 -0500

    Update ProductEdit.tsx

--- src/components/ProductsComponents/Product/ProductEdit.tsx
+++ src/components/ProductsComponents/Product/ProductEdit.tsx
@@ -1,5 +1,10 @@
-import { IonImg, IonGrid, IonRow, IonCol, IonLabel,IonButton,IonContent,IonPage,useIonAlert} from "@ionic/react";
+import { IonImg, IonGrid, IonRow,IonIcon ,IonCol, IonLabel,IonButton,IonContent,IonPage,useIonAlert} from "@ionic/react";
 import './ProductInfo.css'
+import 'pencil-outline.svg'
+import ProductoList from '../List';
+import ProductoDatos from '../List';
+import { list } from "ionicons/icons";
+import ProductDatos from './ProductDatos';
 
 interface ContainerProps
 {
@@ -10,8 +15,9 @@ interface ContainerProps
 }
 
 const ProductEdit : React.FC<ContainerProps> = (props) => {
+    const [present] = useIonAlert();
     return (
-    <div className="ProductoInfo">
+    <div className="ProductoEdit">
         <IonGrid>
         <IonRow>
             <IonCol size="9">
@@ -28,14 +34,32 @@ const ProductEdit : React.FC<ContainerProps> = (props) => {
             <IonCol size="3">
                 <IonImg src={`{props.image}`} />
             </IonCol>
-            <IonCol size="1">
-            <IonButton>Eliminar</IonButton>
-            <IonButton>Editar</IonButton>
+            <IonCol size="3">
+            <IonButton onClick={() => {
+
+                    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                    list
+                
+                }
+                
+             }expand="block" className="mt-3"
+            >Editar</IonButton>
+            <IonButton
+          expand="block"
+          onClick={() =>
+            present({
+              header: 'Alerta',
+              message: '¿Desea Eliminar?',
+              buttons: ['Cancel', { text: 'Ok', handler: (d) => console.log('ok pressed') }],
+              onDidDismiss: (e) => console.log('did dismiss'),
+            })}>Eliminar</IonButton>
             </IonCol>
+            
             
             
         </IonRow>  
         </IonGrid>
+        
     </div>
     )
 }

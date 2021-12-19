427d3063a20dd5b889b97cc7ed7b1b4bb8d026bf
Raith Medina Villegas <rmedinav@unsa.edu.pe>
2021-12-19 14:57:48 -0500

    Se modifico visualizacion de proudcto

--- src/components/ProductsComponents/ListP.tsx
+++ src/components/ProductsComponents/ListP.tsx
@@ -1,33 +1,81 @@
-import { IonCol, IonLoading, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonNav, IonRow, IonTitle } from '@ionic/react';
-import ProductEdit from '../ProductsComponents/Product/ProductEdit'
+import { IonCol, IonLoading, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonNav, IonRow, IonTitle, IonModal, IonItem, IonInput, IonButton, IonAlert, useIonAlert } from '@ionic/react';
+import ProductEdit from './Product/ProductInfo'
 import { db } from '../../firebaseConfig'
-import { useState, useEffect } from 'react';
-import { collection, getDoc, getDocs, onSnapshot, query, } from "firebase/firestore";
+import { useState, useEffect, useContext } from 'react';
+import { collection, getDoc, getDocs, onSnapshot, query,where } from "firebase/firestore";
+import { SesionContext } from '../../Contexto/Sesion/Context';
+import { actualizarProducto,deleteProducto } from '../../servicios/firebaseProducto';
+
 
 interface ContainerProps
 {
+
 }
 
 const ProductoList: React.FC<ContainerProps> = () => {
 
+
+    const {sesion}=useContext(SesionContext)
+    var idsesion = sesion.uid
+    
+    const [present] = useIonAlert();
+
     const [arrayProductos, setArrayProductos] = useState([{}]);
     const [loading, setLoading] = useState<boolean>(true);
+
+    //boleano a usar
+    const [mActualizarDatos,setboolActualizarDatos] = useState(false);
+    //variables a usar 
+    const [descripcion, setDescripcion] = useState('')
+    const [categoria, setCategoria] = useState('')
+   // const [idtienda, setIdtienda] = useState(idsesion)
+   // const [imagen, setImagen] = useState('')
+    const [nombre, setNombre] = useState('')
+    const [precio, setPrecio] = useState(0)
+    const [stock,setStock]=useState(0)
+    const [uid,setuidDoc] = useState('')
+
+    async function guardarActualizarDatos() {
+        
+          await actualizarProducto(uid,precio,stock)
+          setboolActualizarDatos(false)
+    }
+
+    async function deleteProduct(uid:string) {
+        
+        await deleteProducto(uid)
+     }
+
+    const actualizarDatos=(categoria:string, descripcion:string,nombre: string, precio:number,stock:number,uid:string)=>
+    {   setboolActualizarDatos(true)
+        setuidDoc(uid)
+        setDescripcion(descripcion)
+        setCategoria(categoria)
+        setNombre(nombre)
+        setPrecio(precio)
+        setStock(stock)
+    }
+
 
 
     const productosCollectionRef = collection(db, 'productos')
     useEffect(() => {
         async function obtenerProductos()
         {
-            const data = await getDocs(productosCollectionRef);
-            setArrayProductos(data.docs.map((doc) => (
-                {...doc.data(), 
-                    id: doc.id}
-            )))
-            setLoading(false)
-
+            const p = query(productosCollectionRef, where('idtienda', '==', idsesion))
+            const d = await getDocs(p);
+            setArrayProductos(d.docs.map((doc) => (
+          {
+            ...doc.data(),
+            uid: doc.id
+          }
+        )))
+        setLoading(false)                 
         }
         obtenerProductos()
     }, [loading])
+
+
 
     if (loading) {
         return <IonLoading
@@ -36,24 +84,100 @@ const ProductoList: React.FC<ContainerProps> = () => {
         message={'Cargando...'}
     />
     }
-
     return (
         <IonRow>
+            <IonModal isOpen={mActualizarDatos} >
+            <IonGrid>
+                <IonRow>
+                    <IonCol className='ion-text-center'>
+                        <h2>Actualizar Producto</h2>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol >
+                        <IonItem>
+                            <IonLabel  > Nombre:{nombre}</IonLabel>
+                        </IonItem>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol >
+                        <IonItem>
+                            <IonLabel > Descripcion: {descripcion}</IonLabel>
+                        </IonItem>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol >
+                        <IonItem>
+                            <IonLabel  > Categoria: {categoria}</IonLabel>
+                        </IonItem>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol >
+                        <IonItem>
+                            <IonLabel position="floating"> Precio:{precio}</IonLabel>
+                            <IonInput onIonChange={(e:any) => setPrecio(e.target.value)} />
+                        </IonItem>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol >
+                        <IonItem>
+                            <IonLabel position="floating"> Stock:{stock}</IonLabel>
+                            <IonInput onIonChange={(e:any) => setStock(e.target.value)} />
+                        </IonItem>
+                    </IonCol>
+                </IonRow>
+                
+                <IonRow>
+                    <IonCol className='ion-text-center ion-margin-top'>
+                        <IonButton expand="block" fill="outline" onClick={()=>setboolActualizarDatos(false)} >
+                            Cerrar
+                        </IonButton>
+                        <IonButton expand="block" fill="outline" onClick={()=>{guardarActualizarDatos()}} >
+                            Actualizar
+                        </IonButton>
+
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+            </IonModal>
             <IonCol>
                 <IonContent scrollY={true} fullscreen>
-                    <IonTitle>Productos</IonTitle>
                     <IonGrid>
                         {
                             arrayProductos.length > 0 ?
                             arrayProductos.map((item:any, index:Number) => {
                                 return (
                                     <IonRow key={index.toString()}>
-                                        <IonCol>
-                                            <ProductEdit  productoNombre={item.nombre} docId={item.id} productoSede='' image="" precio={item.precio} />
+                                        <IonCol onClick={()=>actualizarDatos(item.categoria,item.descripcion,item.nombre,item.precio,item.stock,item.uid)} >
+                                            <ProductEdit  productoNombre={item.nombre} docId={item.uid} productoSede='' image="" precio={item.precio} />
                                         </IonCol>
+
+                                        <IonButton
+                                                expand="block"
+                                                onClick={() =>
+                                                    present({
+                                                    cssClass: 'my-css',
+                                                    header: 'Alerta',
+                                                    message: '¿Desea eliminar el producto?',
+                                                    buttons: ['Cancel', 
+                                                    //Borra el producto despues del mensaje de confirmación
+                                                    { text: 'Ok', handler: (d) => /*console.log('did dismiss')*/deleteProduct(item.uid) }],
+                                                    onDidDismiss: (e) => console.log('did dismiss'),
+                                                    })
+                                                }>Eliminar
+                                            </IonButton>
+                                            <IonButton
+                                                expand="block"
+                                                onClick={()=>actualizarDatos(item.categoria,item.descripcion,item.nombre,item.precio,item.stock,item.uid)}>Editar
+                                            </IonButton>
+
                                     </IonRow>
                                 )
-                            }) : null
+                            }) : <p>No Existen Productos</p>
                         }
                     </IonGrid>
                 </IonContent>

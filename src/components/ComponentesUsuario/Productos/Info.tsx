b4788aa13ac7a442be966ac7280d29f0526fba0d
JoseAntonio98 <sawm5798@gmail.com>
2021-12-19 20:20:08 -0500

    Actualización carrito

--- src/components/ComponentesUsuario/Productos/Info.tsx
+++ src/components/ComponentesUsuario/Productos/Info.tsx
@@ -1,38 +1,41 @@
-import { IonImg, IonGrid, IonRow, IonCol, IonLabel} from "@ionic/react";
+import { IonImg, IonGrid, IonRow, IonCol, IonLabel, IonItem, IonTitle } from "@ionic/react";
 import { GeoPoint } from "firebase/firestore";
 import './style/info.css'
 
-interface ProductoProps
-{
-    id : string
-    nombre : string
-    descripcion : string
-    precio : number
-    imagen : string
+interface ProductoProps {
+    id: string
+    nombre: string
+    descripcion: string
+    precio: number
+    imagen: string
+    categoria : string
 }
 
-const ProductInfo : React.FC<ProductoProps> = (props) => {
+const ProductInfo: React.FC<ProductoProps> = (props) => {
     return (
-    <div className="ProductoInfo">
-        <IonGrid>
-        <IonRow>
-            <IonCol size="9">
-                <div>
-                    <strong>{props.nombre}</strong>
-                </div>
-                <div>
-                    <IonLabel>{props.descripcion}</IonLabel>
-                </div>
-                <div>
-                    <IonLabel>{props.precio}</IonLabel>
-                </div>
-            </IonCol>
-            <IonCol offsetLg="1" sizeLg="2">
-                <IonImg src={props.imagen} />
-            </IonCol>
-        </IonRow>  
-        </IonGrid>
-    </div>
+        <div className="ProductoInfo">
+            <IonGrid>
+                <IonRow>
+                    <IonCol size="9">
+                        <div>
+                            <h2>{props.nombre}</h2>
+                        </div>
+                        <div>
+                            <IonLabel>Despcripción: {props.descripcion}</IonLabel>
+                        </div>
+                        <div>
+                            <IonLabel>Categoría: {props.categoria}</IonLabel>
+                        </div>
+                        <div>
+                            <IonLabel>Precio: S./<strong>{props.precio}</strong> por unidad</IonLabel>
+                        </div>
+                    </IonCol>
+                    <IonCol offsetLg="1" sizeLg="2">
+                        <IonImg src={props.imagen} />
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+        </div>
     )
 }
 
